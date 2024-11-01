dd286a4703c54f143afd667d04ff24a54aa89293
ghost <2990955050@qq.com>
2024-11-01 10:37:33 +0800

    pwm: sophgo: add driver for SG2044 on top of SG2042

--- drivers/pwm/pwm-sophgo-sg2042.c
+++ drivers/pwm/pwm-sophgo-sg2042.c
@@ -25,34 +25,37 @@
 
 #include <asm/div64.h>
 
-/*
- * Offset RegisterName
- * 0x0000 HLPERIOD0
- * 0x0004 PERIOD0
- * 0x0008 HLPERIOD1
- * 0x000C PERIOD1
- * 0x0010 HLPERIOD2
- * 0x0014 PERIOD2
- * 0x0018 HLPERIOD3
- * 0x001C PERIOD3
- * Four groups and every group is composed of HLPERIOD & PERIOD
- */
+#define REG_HLPERIOD		0x0
+#define REG_PERIOD		0x4
+#define REG_GROUP		0x8
+#define REG_POLARITY		0x40
+
+#define REG_PWMSTART		0x44
+#define REG_PWMUPDATE		0x4C
+#define REG_SHIFTCOUNT		0x80
+#define REG_SHIFTSTART		0x90
+#define REG_PWM_OE		0xD0
+
+#define PWM_MINPERIOD		2
+#define PWM_MINHLPERIOD		1
+#define PWM_REG_NUM		0x80
+
 #define SG2042_HLPERIOD(chan) ((chan) * 8 + 0)
 #define SG2042_PERIOD(chan) ((chan) * 8 + 4)
 
 #define SG2042_PWM_CHANNELNUM	4
 
 /**
- * struct sg2042_pwm_ddata - private driver data
+ * struct pwm_ddata - private driver data
  * @base:		base address of mapped PWM registers
  * @clk_rate_hz:	rate of base clock in HZ
  */
-struct sg2042_pwm_ddata {
+struct pwm_ddata {
 	void __iomem *base;
 	unsigned long clk_rate_hz;
 };
 
-static void pwm_sg2042_config(void __iomem *base, unsigned int chan, u32 period, u32 hlperiod)
+static void pwm_sophgo_config(void __iomem *base, unsigned int chan, u32 period, u32 hlperiod)
 {
 	writel(period, base + SG2042_PERIOD(chan));
 	writel(hlperiod, base + SG2042_HLPERIOD(chan));
@@ -61,7 +64,7 @@ static void pwm_sg2042_config(void __iomem *base, unsigned int chan, u32 period,
 static int pwm_sg2042_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 			    const struct pwm_state *state)
 {
-	struct sg2042_pwm_ddata *ddata = pwmchip_get_drvdata(chip);
+	struct pwm_ddata *ddata = pwmchip_get_drvdata(chip);
 	u32 hlperiod;
 	u32 period;
 
@@ -69,7 +72,7 @@ static int pwm_sg2042_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 		return -EINVAL;
 
 	if (!state->enabled) {
-		pwm_sg2042_config(ddata->base, pwm->hwpwm, 0, 0);
+		pwm_sophgo_config(ddata->base, pwm->hwpwm, 0, 0);
 		return 0;
 	}
 
@@ -88,7 +91,7 @@ static int pwm_sg2042_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 	dev_dbg(pwmchip_parent(chip), "chan[%u]: period=%u, hlperiod=%u\n",
 		pwm->hwpwm, period, hlperiod);
 
-	pwm_sg2042_config(ddata->base, pwm->hwpwm, period, hlperiod);
+	pwm_sophgo_config(ddata->base, pwm->hwpwm, period, hlperiod);
 
 	return 0;
 }
@@ -96,7 +99,7 @@ static int pwm_sg2042_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 static int pwm_sg2042_get_state(struct pwm_chip *chip, struct pwm_device *pwm,
 				struct pwm_state *state)
 {
-	struct sg2042_pwm_ddata *ddata = pwmchip_get_drvdata(chip);
+	struct pwm_ddata *ddata = pwmchip_get_drvdata(chip);
 	unsigned int chan = pwm->hwpwm;
 	u32 hlperiod;
 	u32 period;
@@ -117,13 +120,88 @@ static int pwm_sg2042_get_state(struct pwm_chip *chip, struct pwm_device *pwm,
 	return 0;
 }
 
+static int pwm_sg2044_apply(struct pwm_chip *chip, struct pwm_device *pwm,
+			    const struct pwm_state *state)
+{
+	struct pwm_ddata *ddata = pwmchip_get_drvdata(chip);
+	uint32_t pwm_value;
+	u32 hlperiod;
+	u32 period;
+
+	if (state->polarity == PWM_POLARITY_INVERSED)
+		return -EINVAL;
+
+	if (!state->enabled) {
+		pwm_sophgo_config(ddata->base, pwm->hwpwm, PWM_MINPERIOD, PWM_MINHLPERIOD);
+		return 0;
+	}
+
+	/*
+	 * Period of High level (duty_cycle) = HLPERIOD x Period_clk
+	 * Period of One Cycle (period) = PERIOD x Period_clk
+	 */
+	period = min(mul_u64_u64_div_u64(ddata->clk_rate_hz, state->period, NSEC_PER_SEC), U32_MAX);
+	hlperiod = min(mul_u64_u64_div_u64(ddata->clk_rate_hz, state->duty_cycle, NSEC_PER_SEC), U32_MAX);
+
+	if (hlperiod > period) {
+		dev_err(pwmchip_parent(chip), "period < hlperiod, failed to apply current setting\n");
+		return -EINVAL;
+	}
+
+	dev_dbg(pwmchip_parent(chip), "chan[%u]: period=%u, hlperiod=%u\n",
+		pwm->hwpwm, period, hlperiod);
+
+	pwm_sophgo_config(ddata->base, pwm->hwpwm, period, hlperiod);
+
+	pwm_value = readl(ddata->base + REG_PWMSTART);
+
+	writel(pwm_value & (~(1 << (pwm->hwpwm))), ddata->base + REG_PWMSTART);
+
+	pwm_value |= (1 << pwm->hwpwm);
+
+	writel(pwm_value, ddata->base + REG_PWM_OE);
+	writel(pwm_value, ddata->base + REG_PWMSTART);
+
+	return 0;
+}
+
+static int pwm_sg2044_get_state(struct pwm_chip *chip, struct pwm_device *pwm,
+				struct pwm_state *state)
+{
+	struct pwm_ddata *ddata = pwmchip_get_drvdata(chip);
+	unsigned int chan = pwm->hwpwm;
+	u32 hlperiod;
+	u32 period;
+
+	period = readl(ddata->base + REG_GROUP * chan + REG_PERIOD);
+	hlperiod = readl(ddata->base + REG_GROUP * chan + REG_HLPERIOD);
+
+	if (!period && !hlperiod)
+		state->enabled = false;
+	else
+		state->enabled = true;
+
+	state->period = DIV_ROUND_UP_ULL((u64)period * NSEC_PER_SEC, ddata->clk_rate_hz);
+	state->duty_cycle = DIV_ROUND_UP_ULL((u64)hlperiod * NSEC_PER_SEC, ddata->clk_rate_hz);
+
+	state->polarity = PWM_POLARITY_NORMAL;
+
+	return 0;
+}
+
 static const struct pwm_ops pwm_sg2042_ops = {
 	.apply = pwm_sg2042_apply,
 	.get_state = pwm_sg2042_get_state,
 };
 
+static const struct pwm_ops pwm_sg2044_ops = {
+	.apply = pwm_sg2044_apply,
+	.get_state = pwm_sg2044_get_state,
+};
+
 static const struct of_device_id sg2042_pwm_ids[] = {
 	{ .compatible = "sophgo,sg2042-pwm" },
+	{ .compatible = "sophgo,sg2044-pwm", .data = &pwm_sg2044_ops },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, sg2042_pwm_ids);
@@ -131,7 +209,7 @@ MODULE_DEVICE_TABLE(of, sg2042_pwm_ids);
 static int pwm_sg2042_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct sg2042_pwm_ddata *ddata;
+	struct pwm_ddata *ddata;
 	struct reset_control *rst;
 	struct pwm_chip *chip;
 	struct clk *clk;
