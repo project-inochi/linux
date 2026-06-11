2da2cbf7005283671f835bbc7b3b9ca0ae4c09dd
Inochi Amaoto <inochiama@gmail.com>
2026-06-11 13:40:02 +0800

    fix buffer
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- arch/riscv/kvm/vcpu_exit.c
+++ arch/riscv/kvm/vcpu_exit.c
@@ -199,6 +199,9 @@ int kvm_riscv_vcpu_exit(struct kvm_vcpu *vcpu, struct kvm_run *run,
 	if (trap->scause & CAUSE_IRQ_FLAG)
 		return 1;
 
+	/* flush dirty log befor any exception */
+	kvm_riscv_vcpu_flush_dirty_buffer(vcpu);
+
 	/* Handle guest traps */
 	ret = -EFAULT;
 	run->exit_reason = KVM_EXIT_UNKNOWN;
